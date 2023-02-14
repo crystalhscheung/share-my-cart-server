e69b9f88d1e3bb16287f740eb0b1b553152369ed
Hiu Suet Cheung <ch.suet@hotmail.com>
2023-02-14 13:25:50 -0500

    fix env file

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 module.exports = {
   client: "mysql",
   connection: {
-    host: "127.0.0.1",
-    database: process.env.DB_LOCAL_DBNAME,
-    user: process.env.DB_LOCAL_USER,
-    password: process.env.DB_LOCAL_PASSWORD,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   },
 };
